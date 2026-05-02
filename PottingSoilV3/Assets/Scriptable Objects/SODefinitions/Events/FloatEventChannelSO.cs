f159242b71b958a14f29cfdf173c8da638cf705e
TLgaming294 <q.kostikov@gmail.com>
2026-05-02 19:36:51 +0200

    Added V3

---
+++ PottingSoilV3/Assets/Scriptable Objects/SODefinitions/Events/FloatEventChannelSO.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(menuName = "Events/Float Event Channel")]
+public class FloatEventChannelSO : ScriptableObject
+{
+    public event Action<float> OnEventRaised;
+
+    public void RaiseEvent(float value)
+    {
+        OnEventRaised?.Invoke(value);
+    }
+}
