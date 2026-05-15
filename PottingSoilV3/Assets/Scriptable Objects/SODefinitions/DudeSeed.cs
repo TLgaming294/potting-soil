5f8dbbf977280848b102e7937783c697d9acf4b5
TLgaming294 <q.kostikov@gmail.com>
2026-05-15 05:54:11 +0200

    added the currency view and dude view prefabs

--- PottingSoilV3/Assets/Scriptable Objects/SODefinitions/DudeSeed.cs
+++ PottingSoilV3/Assets/Scriptable Objects/SODefinitions/DudeSeed.cs
@@ -6,7 +6,7 @@ public class DudeSeed : ScriptableObject
 {
 	[field: SerializeField] public string DudeName { get; set; }
 	[field: SerializeField] public double Cost { get; set; }
-	[field: SerializeField] public CurrencyType Currency { get; set; }
+	[field: SerializeField] public CurrencySO Currency { get; set; }
 	[field: SerializeField] public float GrowthTime { get; set; }
 	[field: SerializeField] public float TimeToLive { get; set; }
 	[field: SerializeField] public Sprite DudeSprite { get; set; }
--- PottingSoilV3/Assets/Scriptable Objects/SODefinitions/Events/CurrencyEventChannel.cs
+++ PottingSoilV3/Assets/Scriptable Objects/SODefinitions/Events/CurrencyEventChannel.cs
@@ -4,9 +4,9 @@ using System;
 [CreateAssetMenu(menuName = "Events/Currency Event Channel")]
 public class CurrencyEventChannel : ScriptableObject
 {
-	public event Action<Currency> OnEventRaised;
+	public event Action<LiveCurrency> OnEventRaised;
 
-	public void RaiseEvent(Currency value)
+	public void RaiseEvent(LiveCurrency value)
 	{
 		OnEventRaised?.Invoke(value);
 	}
